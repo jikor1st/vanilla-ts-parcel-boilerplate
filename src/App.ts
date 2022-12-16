fe3d33062d3f049f0edbe74cd53b8391fdf16410
jikor1st <jikor1st@naver.com>
2022-12-17 01:50:34 +0900

    feat: Parcel settings

---
+++ src/App.ts
@@ -0,0 +1,1 @@
+export const App = `<div><p>Load done</p></div>`;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { App } from "./App";
+
+const root = document.querySelector("#root") as HTMLElement;
+root.innerHTML = App;
